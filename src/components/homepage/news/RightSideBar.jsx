f6ddfbd506da9840aa394c8e1e5bd969f493f612
toufiq178 <tajahmedtoufiq1@gmail.com>
2026-04-26 01:15:10 +0600

    Social Sign-In (Google And Github)

--- src/components/homepage/news/RightSideBar.jsx
+++ src/components/homepage/news/RightSideBar.jsx
@@ -1,26 +1,37 @@
-import Link from "next/link";
+"use client";
+import { authClient } from "@/lib/auth-client";
 import React from "react";
 import { FaGithub, FaGoogle } from "react-icons/fa6";
 
 const RightSideBar = () => {
+  const handleGoogleLogin = async () => {
+    const data = await authClient.signIn.social({
+      provider: "google",
+    });
+  };
+  const handleGithubLogin = async () => {
+    const data = await authClient.signIn.social({
+      provider: "github",
+    });
+  };
   return (
     <div className="space-y-5">
       <h1 className="text-lg font-semibold">Login with</h1>
       <div className="flex flex-col justify-center items-center gap-2">
-        <Link
-          href={""}
+        <button
+          onClick={handleGoogleLogin}
           className="btn  w-full border-blue-400 border-2 py-5  text-blue-500 "
         >
           <FaGoogle />
-          <button> Login with google</button>
-        </Link>
-        <Link
-          href={""}
+          <p> Login with google</p>
+        </button>
+        <button
+          onClick={handleGithubLogin}
           className="btn  w-full border-black border-2"
         >
           <FaGithub />
-          <button> Login with github</button>
-        </Link>
+          <p> Login with github</p>
+        </button>
       </div>
     </div>
   );
--- src/lib/auth.js
+++ src/lib/auth.js
@@ -13,4 +13,14 @@ export const auth = betterAuth({
     // Optional: if you don't provide a client, database transactions won't be enabled.
     client,
   }),
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    },
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    },
+  },
 });
